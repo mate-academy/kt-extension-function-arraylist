98df53578e70e01403db25ae6ee5e5728793c4b8
Nonzeq <bgdn.kobylchak@gmail.com>
2024-06-02 15:08:21 +0300

    added

--- src/main/kotlin/mate/academy/NumbersService.kt
+++ src/main/kotlin/mate/academy/NumbersService.kt
@@ -1,6 +1,5 @@
 package mate.academy
 
-// provide your solution below
 private const val MESSAGE = "The list contains %s elements."
 fun <T> ArrayList<T>.getSizeMessage(): String {
     return MESSAGE.format(this.size)
