3eca5cf482fe71a436158f4852fbd9a885aea231
Anatolii Nazarov <anatoly_n@ukr.net>
2024-12-11 13:16:41 +0200

    For check

--- src/main/kotlin/mate/academy/NumbersService.kt
+++ src/main/kotlin/mate/academy/NumbersService.kt
@@ -1,11 +1,16 @@
 package mate.academy
 
+const val ONE = 10
+const val TWO = 20
+const val THREE = 30
+const val FOUR = 40
+const val FIVE = 50
 //fun ArrayList<Int>.getSizeMessage() : String = "The list contains + ${this.size} + elements."
 fun <E> ArrayList<E>.getSizeMessage(): String {
     return "The list contains ${this.size} elements."
 }
 
 fun main() {
-    val list : ArrayList<Int> = arrayListOf<Int>(10, 20, 30, 40, 50)
+    val list : ArrayList<Int> = arrayListOf<Int>(ONE, TWO, THREE, FOUR, FIVE)
     println(list.getSizeMessage())
 }
